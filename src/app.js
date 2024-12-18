b77253b3ccb8e8d5b3e1aee1f6ab2dc331d2eeed
francoaguilar622 <123565045+francoaguilar622@users.noreply.github.com>
2024-12-18 23:23:08 +0000

    proyeco terminado

--- src/app.js
+++ src/app.js
@@ -6,22 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  var who = ["The dog", "My grandma", "The mailman", "My bird"];
-  var action = ["ate", "peed", "crushed", "broke"];
-  var what = ["my homework", "my phone", "the car"];
-  var when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class ",
+    "when I was sleeping ",
+    "while I was exercising ",
+    "during my lunch ",
+    "while I was praying "
   ];
 
-  var rdm1 = Math.floor(Math.random() * who.length);
-  var rdm2 = Math.floor(Math.random() * action.length);
-  var rdm3 = Math.floor(Math.random() * what.length);
-  var rdm4 = Math.floor(Math.random() * when.length);
+  let rdm1 = Math.floor(Math.random() * who.length);
+  let rdm2 = Math.floor(Math.random() * action.length);
+  let rdm3 = Math.floor(Math.random() * what.length);
+  let rdm4 = Math.floor(Math.random() * when.length);
 
-  document.querySelector("#theexcuse").innerHTML =
-    first + who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4];
+  document.querySelector("#excuse").innerHTML =
+    who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4];
 };
